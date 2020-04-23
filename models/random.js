43954b857aee21ec53ea3eb0d9fe9081daae181c
Srujan Deshpande <srujan.deshpande@gmail.com>
2020-04-23 14:11:32 +0530

    models

--- models/random.js
+++ models/random.js
@@ -1,15 +1,15 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-//create schema for todo
-const TodoSchema = new Schema({
+//create schema for Random number
+const RandomSchema = new Schema({
   action: {
     type: String,
     required: [true, 'The todo text field is required']
   }
 })
 
-//create model for todo
-const Todo = mongoose.model('todo', TodoSchema);
+//create model for Random
+const Random = mongoose.model('random', RandomSchema);
 
-module.exports = Todo;
+module.exports = Random;
