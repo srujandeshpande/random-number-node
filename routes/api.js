15e6ac431712ff54d0ec2fa7d53875bb19b7cb65
Srujan Deshpande <srujan.deshpande@gmail.com>
2020-04-24 13:59:58 +0530

    index

--- routes/api.js
+++ routes/api.js
@@ -1,16 +1,31 @@
 const express = require ('express');
 const router = express.Router();
+const Random = require('../models/random');
 
-router.get('/todos', (req, res, next) => {
+router.get('/random', (req, res, next) => {
 
+  //this will return all the data, exposing only the id and action field to the client
+  Random.find({}, 'action')
+    .then(data => res.json(data))
+    .catch(next)
 });
 
-router.post('/todos', (req, res, next) => {
-
+router.post('/random', (req, res, next) => {
+  if(req.body.action){
+    Random.create(req.body)
+      .then(data => res.json(data))
+      .catch(next)
+  }else {
+    res.json({
+      error: "The input field is empty"
+    })
+  }
 });
 
-router.delete('/todos/:id', (req, res, next) => {
-
+router.delete('/random/:id', (req, res, next) => {
+  Random.findOneAndDelete({"_id": req.params.id})
+    .then(data => res.json(data))
+    .catch(next)
 })
 
 module.exports = router;
