ba006e1ccbfcfe5d8a3d0389f3276c62d39e9842
Srujan Deshpande <srujan.deshpande@gmail.com>
2020-05-04 20:31:52 +0530

    mongo purge

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-//const mongoose = require('mongoose');
 const routes = require('./routes/api');
 const path = require('path');
 require('dotenv').config();
@@ -8,14 +7,6 @@ require('dotenv').config();
 const app = express();
 
 const port = process.env.PORT || 5000;
-
-//connect to the database
-//mongoose.connect(process.env.DB, { useNewUrlParser: true })
-//  .then(() => console.log(`Database connected successfully`))
-//  .catch(err => console.log(err));
-
-//since mongoose promise is depreciated, we overide it with node's promise
-//mongoose.Promise = global.Promise;
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
