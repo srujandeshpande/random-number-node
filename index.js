40fd0b6b79b304e6c0acb8226e3b3b3f9fe609f7
Srujan Deshpande <srujan.deshpande@gmail.com>
2020-05-04 20:29:42 +0530

    random

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+//const mongoose = require('mongoose');
 const routes = require('./routes/api');
 const path = require('path');
 require('dotenv').config();
@@ -10,12 +10,12 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 //connect to the database
-mongoose.connect(process.env.DB, { useNewUrlParser: true })
-  .then(() => console.log(`Database connected successfully`))
-  .catch(err => console.log(err));
+//mongoose.connect(process.env.DB, { useNewUrlParser: true })
+//  .then(() => console.log(`Database connected successfully`))
+//  .catch(err => console.log(err));
 
 //since mongoose promise is depreciated, we overide it with node's promise
-mongoose.Promise = global.Promise;
+//mongoose.Promise = global.Promise;
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
--- routes/api.js
+++ routes/api.js
@@ -1,11 +1,11 @@
 const express = require ('express');
 const router = express.Router();
-const Todo = require('../models/random');
+const Random = require('../models/random');
 
 router.get('/todos', (req, res, next) => {
 
   //this will return all the data, exposing only the id and action field to the client
-  Todo.find({}, 'action')
+  Random.find({}, 'action')
     .then(data => res.json(data))
     .catch(next)
 });
